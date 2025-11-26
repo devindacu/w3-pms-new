0b6b56367cdf034fe26a6f23c3c502e2846b96c3
Devinda Udayasiri <w3mediaagency@gmail.com>
2025-11-26 07:51:53 +0000

    Generated by Spark: Remove $ symbol from entire system and keep one currency now LKR ( Sri Lankan Ruppee )

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -77,7 +77,9 @@ export function formatTime(timestamp: number | undefined): string {
 export function formatCurrency(amount: number): string {
   return new Intl.NumberFormat('en-LK', {
     style: 'currency',
-    currency: 'LKR'
+    currency: 'LKR',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
   }).format(amount)
 }
 
