eb1bc0abe1c7a25220e2165a3ea8ada3763d7fe8
Devinda Udayasiri <w3mediaagency@gmail.com>
2026-02-04 19:04:28 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/adapters/index.ts
+++ src/components/adapters/index.ts
@@ -1,2 +1,4 @@
 export { DialogAdapter, LoadingDialog, ConfirmDialog } from './DialogAdapter'
 export type { DialogAdapterProps, LoadingDialogProps, ConfirmDialogProps } from './DialogAdapter'
+
+export default DialogAdapter
